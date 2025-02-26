09aa3845cb204ef9303bf728aa7755089354d588
GabrielSchranck <gabriel.futurisss@gmail.com>
2025-02-26 00:47:09 -0300

    Modificando estrurura do projeto angular

--- Projeto experimental/AppAngular/src/app/pages/registro/registro.component.ts
+++ Projeto experimental/AppAngular/src/app/pages/registro/registro.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { UseracessComponent } from '../../components/useracess/useracess.component';
+import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [UseracessComponent, RouterModule, ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+
+  formularioRegistro = new FormGroup({
+    nome: new FormControl(''),
+    email: new FormControl(''),
+    cpf: new FormControl(''),
+    dataNascimento: new FormControl(''),
+    genero: new FormControl(''),  
+    ddd: new FormControl(''),
+    contato: new FormControl(''),
+    senha: new FormControl('')
+  });
+
+  opcoesGenero = [
+    {valor: "masculino", texto: "Masculino"},
+    {valor: "feminino", texto: "Feminino"},
+    {valor: "outro", texto: "Outro"}
+  ];
 
 }
---
+++ Projeto experimental/AppAngular/src/app/pages/userbooks/userbooks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userbooks',
+  imports: [],
+  templateUrl: './userbooks.component.html',
+  styleUrl: './userbooks.component.css'
+})
+export class UserbooksComponent {
+
+}
