e5960c89f53cb47a3ab1d9efe8aae59431f8810a
Gabriel <gabriel.futurisss@gmail.com>
2025-02-28 11:11:15 -0300

    Adicionando Token JWT

--- Projeto experimental/BookAPI/BookAPI/Program.cs
+++ Projeto experimental/BookAPI/BookAPI/Program.cs
@@ -1,7 +1,10 @@
 using BookAPI.Data;
 using BookAPI.Repositories.Clientes;
 using BookAPI.Repositories.Livros;
+using BookAPI.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,36 @@ builder.Services.AddDbContext<BookDbContext>(options =>
 
 builder.Services.AddScoped<ILivroRepository, LivroRepository>();
 builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+			 .AddJwtBearer(option =>
+			 {
+				 option.TokenValidationParameters = new TokenValidationParameters
+				 {
+					 ValidateIssuer = false,
+					 ValidateAudience = false,
+					 ValidateLifetime = true,
+					 ValidateIssuerSigningKey = true,
+
+					 ValidIssuer = "Teste.Securiry.Bearer",
+					 ValidAudience = "Teste.Securiry.Bearer",
+					 IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+				 };
+
+				 option.Events = new JwtBearerEvents
+				 {
+					 OnAuthenticationFailed = context =>
+					 {
+						 Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+						 return Task.CompletedTask;
+					 },
+					 OnTokenValidated = context =>
+					 {
+						 Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+						 return Task.CompletedTask;
+					 }
+				 };
+			 });
 
 builder.Services.AddCors(options =>
 {
@@ -44,6 +77,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ Projeto experimental/BookAPI/BookAPI/Token/JwtSecurityKey.cs
@@ -0,0 +1,11 @@
+﻿namespace BookAPI.Token;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+public class JwtSecurityKey
+{
+	public static SymmetricSecurityKey Create(string secret)
+	{
+		return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+	}
+}
