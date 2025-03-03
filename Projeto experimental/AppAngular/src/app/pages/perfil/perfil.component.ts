4a0019ff73fbd16a00a16068df3a19ece9e2c8b4
Gabriel <gabriel.futurisss@gmail.com>
2025-03-03 15:07:22 -0300

    Implementando components de perfil

--- Projeto experimental/AppAngular/src/app/pages/perfil/perfil.component.ts
+++ Projeto experimental/AppAngular/src/app/pages/perfil/perfil.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators  } from '@angular/forms';
+import { cpf } from 'cpf-cnpj-validator';
+import { Cliente } from '../../core/models/Cliente';
+import { ClienteService } from '../../core/services/cliente.service';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+
+  formularioPerfil: any;
+
+  constructor(private clienteService: ClienteService) { }
+
+  ngOnInit(): void {
+    this.formularioPerfil = new FormGroup({
+      nome: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      cpf: new FormControl(''),
+      idade: new FormControl(''),
+      dataNascimento: new FormControl(''),
+      genero: new FormControl(''),
+      ddd: new FormControl(''),
+      contato: new FormControl(''),
+    });
+  }
+
+  private async getPerfil(id: number): Promise<Cliente> {
+    const cliente = await this.clienteService.GetById(id).toPromise();
+    if (!cliente) {
+      throw new Error('Cliente not found');
+    }
+    return cliente;
+  }
+
 
 }
