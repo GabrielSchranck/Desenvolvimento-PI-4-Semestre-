9275098133bc7a3c53f7c5ddbbaee82e1ece0dac
ChristianSRocha <chris.rocha.santos@hotmail.com>
2025-04-26 17:31:17 -0300

    Alteração em banner e adicionado imagens

--- EmprestaAi/EmprestaAiAp/src/app/MainPages/home/home.component.ts
+++ EmprestaAi/EmprestaAiAp/src/app/MainPages/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavbarComponent } from "../../Shered/navbar/navbar.component";
-
 
 @Component({
   selector: 'app-home',
@@ -8,6 +7,23 @@ import { NavbarComponent } from "../../Shered/navbar/navbar.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  currentSlide = 0;
+  private slideInterval: any;
 
+  ngOnInit() {
+    this.startSlideShow();
+  }
+
+  ngOnDestroy() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  private startSlideShow() {
+    this.slideInterval = setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % 4;
+    }, 5000);
+  }
 }
